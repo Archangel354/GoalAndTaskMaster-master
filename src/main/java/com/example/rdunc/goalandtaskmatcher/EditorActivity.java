ab9d99a63ce787973c8caa73ae5de71b518b3b54
Archangel354 <rduncan354@gmail.com>
2018-04-22 16:34:56 -0400

    Starting build the EditorActivity.java

--- src/main/java/com/example/rdunc/goalandtaskmatcher/EditorActivity.java
+++ src/main/java/com/example/rdunc/goalandtaskmatcher/EditorActivity.java
@@ -1,14 +1,52 @@
 package com.example.rdunc.goalandtaskmatcher;
 
+import android.app.LoaderManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.AnyRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    /**
+     * Identifier for the goal data loader
+     */
+    private static final int EXISTING_GOAL_LOADER = 0;
+
+    /**
+     * Content URI for the existing goal (null if it's a new goal)
+     */
+    private Uri mCurrentGoalUri;
+
+    /**
+     * EditText field to enter the goal's description
+     */
+    private EditText mDescriptionEditText;
+
+    /**
+     * Boolean flag that keeps track of whether the goal has been edited (true) or not (false)
+     */
+    private boolean mGoalHasChanged = false;
+
+    /**
+     * OnTouchListener that listens for any user touches on a View, implying that they are modifying
+     * the view, and we change the mGoalHasChanged boolean to true.
+     */
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mGoalHasChanged = true;
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +61,20 @@ public class EditorActivity extends AppCompatActivity {
                 + '/' + context.getResources().getResourceTypeName(drawableId)
                 + '/' + context.getResources().getResourceEntryName(drawableId) );
         return imageUri;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
