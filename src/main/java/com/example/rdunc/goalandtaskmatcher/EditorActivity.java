d90087c5847aa40870b30fe865b1e19c499c65bc
Archangel354 <rduncan354@gmail.com>
2018-04-22 11:46:19 -0400

    Updating the MainActivity with new imports and EditorActivity with getUriToDrawable

--- src/main/java/com/example/rdunc/goalandtaskmatcher/EditorActivity.java
+++ src/main/java/com/example/rdunc/goalandtaskmatcher/EditorActivity.java
@@ -1,5 +1,10 @@
 package com.example.rdunc.goalandtaskmatcher;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,14 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    public static final Uri getUriToDrawable(@NonNull Context context,
+                                             @AnyRes int drawableId) {
+        Uri imageUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + context.getResources().getResourcePackageName(drawableId)
+                + '/' + context.getResources().getResourceTypeName(drawableId)
+                + '/' + context.getResources().getResourceEntryName(drawableId) );
+        return imageUri;
     }
 }
--- src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
+++ src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
@@ -1,7 +1,26 @@
 package com.example.rdunc.goalandtaskmatcher;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.example.rdunc.goalandtaskmatcher.data.GoalandTaskMatcherContract.GoalEntry;
+
+import static com.example.rdunc.goalandtaskmatcher.EditorActivity.getUriToDrawable;
 
 public class MainActivity extends AppCompatActivity {
 
