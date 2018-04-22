30d269663fba49af864d515812239b757a1cb7fc
Archangel354 <rduncan354@gmail.com>
2018-04-22 12:00:08 -0400

    Now Big Green button will open EditorActivity

--- src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
+++ src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
@@ -17,16 +17,45 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
-
 import com.example.rdunc.goalandtaskmatcher.data.GoalandTaskMatcherContract.GoalEntry;
-
 import static com.example.rdunc.goalandtaskmatcher.EditorActivity.getUriToDrawable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    // Identifier for the GoalAndTaskMaster data loader
+    private static final int GOAL_LOADER = 0;
+
+    //* Adapter for the ListView
+    GoalCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup big green "plus" button to open EditorActivity
+        ImageButton btnAddGoal = (ImageButton) findViewById(R.id.btnAddGoal);
+        btnAddGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
