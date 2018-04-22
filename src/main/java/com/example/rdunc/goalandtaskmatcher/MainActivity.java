3ad6bd8cc9ce43434a5750f533ed593660f616c5
Archangel354 <rduncan354@gmail.com>
2018-04-22 12:55:50 -0400

    OMG!  I can really add to data to the list!  MainActivity is now complete.  Do I really need to create an EditorActivity?

--- src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
+++ src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
@@ -104,18 +104,64 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         Uri newUri = getContentResolver().insert(GoalEntry.CONTENT_URI, values);
     }
 
+    /**
+     * Helper method to delete all goals in the database.
+     */
+    private void deleteAllGoals() {
+        int rowsDeleted = getContentResolver().delete(GoalEntry.CONTENT_URI, null, null);
+        Log.v("MainActivity", rowsDeleted + " rows deleted from products database");
+    }
+
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertGoal();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteAllGoals();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                GoalEntry.MY_GOAL_ID,
+                GoalEntry.COLUMN_GOAL_DESCRIPTION
+
+        };
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                GoalEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+        // Update {@link PetCursorAdapter} with this new cursor containing updated product data
+        mCursorAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
     }
 }
