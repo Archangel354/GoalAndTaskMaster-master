ca5b99be3491ea4cd339fc506f694ba0c13d7554
Archangel354 <rduncan354@gmail.com>
2018-04-23 16:45:15 -0400

    Added alot to the EditorActivity

--- src/main/java/com/example/rdunc/goalandtaskmatcher/EditorActivity.java
+++ src/main/java/com/example/rdunc/goalandtaskmatcher/EditorActivity.java
@@ -2,6 +2,7 @@ package com.example.rdunc.goalandtaskmatcher;
 
 import android.app.LoaderManager;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
@@ -11,9 +12,15 @@ import android.support.annotation.AnyRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.rdunc.goalandtaskmatcher.data.GoalandTaskMatcherContract;
 
 public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -79,7 +86,100 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
         // Find all relevant views that we will need to read user input from
         mDescriptionEditText = (EditText) findViewById(R.id.edtGoalDescription);
+
+        // Setup OnTouchListeners on all the input fields, so we can determine if the user
+        // has touched or modified them. This will let us know if there are unsaved changes
+        // or not, if the user tries to leave the editor without saving.
+        mDescriptionEditText.setOnTouchListener(mTouchListener);
     }
+
+    /**
+     * Get user input from editor and save goal into goal database.
+     */
+    private void saveGoal() {
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        String descriptionString = mDescriptionEditText.getText().toString().trim();
+
+
+        // Check if this is supposed to be a new product
+        // and check if all the fields in the editor are blank
+        if (mCurrentGoalUri == null && (TextUtils.isEmpty(descriptionString) )) {
+            Toast.makeText(this, "Please add info to all the fields",
+                    Toast.LENGTH_SHORT).show();
+            // Since no fields were modified, we can return early without creating a new goal.
+            // No need to create ContentValues and no need to do any ContentProvider operations.
+            return;
+        }
+
+        // Create a ContentValues object where column names are the keys,
+        // and goal attributes from the editor are the values.
+        ContentValues values = new ContentValues();
+        values.put(GoalandTaskMatcherContract.GoalEntry.COLUMN_GOAL_DESCRIPTION, descriptionString);
+
+        // Determine if this is a new or existing goal by checking if mCurrentGoalUri is null or not
+        if (mCurrentGoalUri == null) {
+            // This is a NEW goal, so insert a new goal into the provider,
+            // returning the content URI for the new goal.
+            Uri newUri = getContentResolver().insert(GoalandTaskMatcherContract.GoalEntry.CONTENT_URI, values);
+
+            // Show a toast message depending on whether or not the insertion was successful.
+            if (newUri == null) {
+                // If the new content URI is null, then there was an error with insertion.
+                Toast.makeText(this, getString(R.string.editor_insert_goal_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the insertion was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_insert_goal_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Otherwise this is an EXISTING goal, so update the goal with content URI: mCurrentGoalUri
+            // and pass in the new ContentValues. Pass in null for the selection and selection args
+            // because mCurrentGoalUri will already identify the correct row in the database that
+            // we want to modify.
+            int rowsAffected = getContentResolver().update(mCurrentGoalUri, values, null, null);
+
+            // Show a toast message depending on whether or not the update was successful.
+            if (rowsAffected == 0) {
+                // If no rows were affected, then there was an error with the update.
+                Toast.makeText(this, getString(R.string.editor_update_goal_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the update was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_update_goal_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    /**
+     * This method is called after invalidateOptionsMenu(), so that the
+     * menu can be updated (some menu items can be hidden or made visible).
+     */
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new product, hide the "Delete" menu item.
+        if (mCurrentGoalUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+        return true;
+    }
+
+
+
+
 
     public static final Uri getUriToDrawable(@NonNull Context context,
                                              @AnyRes int drawableId) {
