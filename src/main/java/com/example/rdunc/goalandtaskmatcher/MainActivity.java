ef9b22af15370b09633ee71bdc4d4f284d29896e
Archangel354 <rduncan354@gmail.com>
2018-04-24 17:44:43 -0400

    Added button to set text box to "Get Superman cape!"

--- src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
+++ src/main/java/com/example/rdunc/goalandtaskmatcher/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.rdunc.goalandtaskmatcher;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
+    // Identifier for the GoalAndTaskMaster data loader
+    private static final int GOAL_LOADER = 1;
+
+    //* Adapter for the ListView
+    GoalCursorAdapter mCursorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btnMidLevel = findViewById(R.id.btnMidLevel);
+        Button btnDisplayGoals = findViewById(R.id.btnDisplayGoals);
+        final TextView txtGoal1 = findViewById(R.id.txtGoal1);
 
         btnMidLevel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +33,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnDisplayGoals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtGoal1.setText("Get a superman cape!");
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
